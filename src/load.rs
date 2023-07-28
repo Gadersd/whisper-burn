ce49ba406401d1070558c2896459ef507cbf6755
Gadersd <gadersd@gmail.com>
2023-07-27 21:06:13 -0400

    Update burn to 0.8.0

--- src/load.rs
+++ src/load.rs
@@ -1,16 +1,4 @@
 
 
-use burn::{
-    module::Module, 
-    nn::{self, conv::{Conv1d, Conv1dConfig, Conv1dPaddingConfig, Conv1dRecord}}, 
-    tensor::{
-        backend::Backend,
-        activation::relu, 
-        Tensor,
-        Int, 
-        Bool, 
-    },
-};
-
 use crate::model::*;
 
